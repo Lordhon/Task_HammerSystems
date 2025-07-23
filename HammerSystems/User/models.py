02fe010cc7a7be08e0a8ffa8218f12b2ac83ccd4
Lordhon <timofejivlev923@gmail.com>
2025-07-23 22:08:04 +0300

    compleat task

--- HammerSystems/User/models.py
+++ HammerSystems/User/models.py
@@ -2,34 +2,6 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def create_user(self, phone_number, password=None, **extra_fields):
-        if not phone_number:
-            raise ValueError('Phone number must be set')
-        user = self.model(phone_number=phone_number, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, phone_number, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(phone_number, password, **extra_fields)
-
-    def get_by_natural_key(self, phone_number):
-        return self.get(phone_number=phone_number)
-
-
 class User (AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=20, unique=True)
     invite_code = models.CharField(max_length=6, unique=True)
@@ -38,7 +10,7 @@ class User (AbstractBaseUser, PermissionsMixin):
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    objects = UserManager()
+
     USERNAME_FIELD = 'phone_number'
 
     def __str__(self):
